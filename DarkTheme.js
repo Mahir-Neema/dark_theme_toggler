4fbdc1fc8ff09857b655af381cc432242f6c5e51
unknown <mohi10503@gmail.com>
2022-06-07 14:54:15 +0530

    adding starting project:

---
+++ DarkTheme.js
@@ -0,0 +1,21 @@
+
+
+function toggleTheme(){
+    document.querySelector(".places").classList.toggle("dark");
+    document.querySelector(".place_card1").classList.toggle("dark_card");
+    document.querySelector(".place_card2").classList.toggle("dark_card");
+    document.querySelector(".place_card3").classList.toggle("dark_card");
+    document.querySelector(".place_card4").classList.toggle("dark_card");
+    document.querySelector(".place_card5").classList.toggle("dark_card");
+    document.querySelector(".place_card6").classList.toggle("dark_card");
+    document.querySelector(".place_card7").classList.toggle("dark_card");
+    document.querySelector(".place_card8").classList.toggle("dark_card");
+    document.querySelector(".place_card9").classList.toggle("dark_card");
+    document.querySelector(".place_card10").classList.toggle("dark_card");
+    document.querySelector(".place_card11").classList.toggle("dark_card");
+    document.querySelector(".place_card12").classList.toggle("dark_card");
+    document.querySelector(".place_card13").classList.toggle("dark_card");
+    document.querySelector(".place_card14").classList.toggle("dark_card");
+    document.querySelector(".place_card15").classList.toggle("dark_card");
+    
+}
